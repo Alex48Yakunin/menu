0233f86b2df6518ce08fa4682bc3e717d1950e1a
Alex48Yakunin <39828189+Alex48Yakunin@users.noreply.github.com>
2021-10-20 15:35:28 +0300

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,10 +3,10 @@ import {NavLink} from 'react-router-dom';
 export default function Menu(){
   return (
     <nav className="menu">
-        <NavLink exact to="/" activeClassName="menu__item-active" className="menu__item">Гоночн​ое такси</NavLink>
-        <NavLink exact to="/drift" activeClassName="menu__item-active" className="menu__item">Дрифт-такси</NavLink>
-        <NavLink exact to="/timeattack" activeClassName="menu__item-active" className="menu__item">Гонка ​​​​​​Time Attack</NavLink>
-        <NavLink exact to="/forza" activeClassName="menu__item-active" className="menu__item">Forza Karting Sochi</NavLink>
+        <NavLink exact to="/" activeClassName="menu__item-active" className="menu__item">Главная</NavLink>
+        <NavLink exact to="/drift" activeClassName="menu__item-active" className="menu__item">Гоночн​ое такси</NavLink>
+        <NavLink exact to="/timeattack" activeClassName="menu__item-active" className="menu__item">Time Attack</NavLink>
+        <NavLink exact to="/forza" activeClassName="menu__item-active" className="menu__item">Forza Karting</NavLink>
     </nav>
   )
 }
